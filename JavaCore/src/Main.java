fa3e0d295f9b58ff3e93f34138cb0ff7ba7609d5
trung381 <130747543+Trung381@users.noreply.github.com>
2024-09-26 13:48:55 +0700

    jdk17 have some issue, so i created new project with jdk23

--- JavaCore/src/Main.java
+++ JavaCore/src/Main.java
@@ -11,3 +11,21 @@ public class Main {
         scanner.close();
     }
 }
+
+# Cấu hình cổng server
+server.port=8081
+
+
+# Cấu hình datasource (cơ sở dữ liệu)
+spring.datasource.url=jdbc:mysql://localhost:3306/mydb
+spring.datasource.username=root
+spring.datasource.password=pass123
+
+
+# Hibernate JPA
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+
+
+# Cấu hình logging
+logging.level.org.springframework=INFO
